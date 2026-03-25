d318c8e252ee5908e3aa3267c7cd707be9c94a04
arkiver <arkiver@protonmail.com>
2026-03-25 19:12:09 +0100

    Version 20260325.01. Initial.

---
+++ pipeline.py
@@ -0,0 +1,429 @@
+# encoding=utf8
+import datetime
+from distutils.version import StrictVersion
+import hashlib
+import os
+import random
+from seesaw.config import realize, NumberConfigValue
+from seesaw.externalprocess import ExternalProcess
+from seesaw.item import ItemInterpolation, ItemValue
+from seesaw.task import SimpleTask, LimitConcurrent
+from seesaw.tracker import GetItemFromTracker, PrepareStatsForTracker, \
+    UploadWithTracker, SendDoneToTracker
+import shutil
+import socket
+import subprocess
+import sys
+import time
+import string
+import re
+
+if sys.version_info[0] < 3:
+    from urllib import unquote
+else:
+    from urllib.parse import unquote
+
+import seesaw
+from seesaw.externalprocess import WgetDownload
+from seesaw.pipeline import Pipeline
+from seesaw.project import Project
+from seesaw.util import find_executable
+
+from tornado import httpclient
+
+import requests
+import json
+
+if StrictVersion(seesaw.__version__) < StrictVersion('0.8.5'):
+    raise Exception('This pipeline needs seesaw version 0.8.5 or higher.')
+
+
+###########################################################################
+# Find a useful Wget+Lua executable.
+#
+# WGET_AT will be set to the first path that
+# 1. does not crash with --version, and
+# 2. prints the required version string
+
+class HigherVersion:
+    def __init__(self, expression, min_version):
+        self._expression = re.compile(expression)
+        self._min_version = min_version
+
+    def search(self, text):
+        for result in self._expression.findall(text):
+            if result >= self._min_version:
+                print('Found version {}.'.format(result))
+                return True
+
+WGET_AT = find_executable(
+    'Wget+AT',
+    HigherVersion(
+        r'(GNU Wget 1\.[0-9]{2}\.[0-9]{1}-at\.[0-9]{8}\.[0-9]{2})[^0-9a-zA-Z\.-_]',
+        'GNU Wget 1.21.3-at.20260319.01'
+    ),
+    [
+        './wget-at',
+        '/home/warrior/data/wget-at-nss'
+    ]
+)
+
+if not WGET_AT:
+    raise Exception('No usable Wget+At found.')
+
+
+###########################################################################
+# The version number of this pipeline definition.
+#
+# Update this each time you make a non-cosmetic change.
+# It will be added to the WARC files and reported to the tracker.
+VERSION = '20260325.01'
+#USER_AGENT = 'Mozilla/5.0 (X11; Linux i686; rv:124.0) Gecko/20100101 Firefox/124.0'
+TRACKER_ID = 'recursive'
+TRACKER_HOST = 'legacy-api.arpa.li'
+MULTI_ITEM_SIZE = 100
+
+
+###########################################################################
+# This section defines project-specific tasks.
+#
+# Simple tasks (tasks that do not need any concurrency) are based on the
+# SimpleTask class and have a process(item) method that is called for
+# each item.
+class CheckIP(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'CheckIP')
+        self._counter = 0
+
+    def process(self, item):
+        # NEW for 2014! Check if we are behind firewall/proxy
+
+        if self._counter <= 0:
+            item.log_output('Checking IP address.')
+            ip_set = set()
+
+            ip_set.add(socket.gethostbyname('twitter.com'))
+            #ip_set.add(socket.gethostbyname('facebook.com'))
+            ip_set.add(socket.gethostbyname('youtube.com'))
+            ip_set.add(socket.gethostbyname('microsoft.com'))
+            ip_set.add(socket.gethostbyname('icanhas.cheezburger.com'))
+            ip_set.add(socket.gethostbyname('archiveteam.org'))
+
+            if len(ip_set) != 5:
+                item.log_output('Got IP addresses: {0}'.format(ip_set))
+                item.log_output(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+                raise Exception(
+                    'Are you behind a firewall/proxy? That is a big no-no!')
+
+        # Check only occasionally
+        if self._counter <= 0:
+            self._counter = 10
+        else:
+            self._counter -= 1
+
+
+class PrepareDirectories(SimpleTask):
+    def __init__(self, warc_prefix):
+        SimpleTask.__init__(self, 'PrepareDirectories')
+        self.warc_prefix = warc_prefix
+
+    def process(self, item):
+        item_name = item['item_name']
+        item_name_hash = hashlib.sha1(item_name.encode('utf8')).hexdigest()
+        escaped_item_name = item_name_hash
+        dirname = '/'.join((item['data_dir'], escaped_item_name))
+
+        if os.path.isdir(dirname):
+            shutil.rmtree(dirname)
+
+        os.makedirs(dirname)
+
+        item['item_dir'] = dirname
+        item['warc_file_base'] = '-'.join([
+            self.warc_prefix,
+            item_name_hash,
+            time.strftime('%Y%m%d-%H%M%S')
+        ])
+
+        open('%(item_dir)s/%(warc_file_base)s.warc.gz' % item, 'w').close()
+        open('%(item_dir)s/%(warc_file_base)s_data.txt' % item, 'w').close()
+
+class MoveFiles(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'MoveFiles')
+
+    def process(self, item):
+        os.rename('%(item_dir)s/%(warc_file_base)s.warc.gz' % item,
+              '%(data_dir)s/%(warc_file_base)s.warc.gz' % item)
+        os.rename('%(item_dir)s/%(warc_file_base)s_data.txt' % item,
+              '%(data_dir)s/%(warc_file_base)s_data.txt' % item)
+
+        shutil.rmtree('%(item_dir)s' % item)
+
+
+def normalize_string(s):
+    while True:
+        temp = unquote(s).strip().lower()
+        if temp == s:
+            break
+        s = temp
+    return s
+
+
+class SortAndSelectItems(SimpleTask):
+    ORDER = []
+    ITEMS = {}
+
+    def __init__(self):
+        SimpleTask.__init__(self, 'SortAndSelectItems')
+
+    @classmethod
+    def handle_items(cls, items, log_output):
+        counts = {}
+        for item in items:
+            item_job, item_value = item.split(':', 1)
+            if item_job not in cls.ITEMS:
+                cls.ORDER.append(item_job)
+                cls.ITEMS[item_job] = []
+            cls.ITEMS[item_job].append(item)
+            if item_job not in counts:
+                counts[item_job] = 0
+            counts[item_job] += 1
+        for k, v in sorted(counts.items()):
+            log_output('Found {} items for job {}.'.format(v, k))
+        selected_job = cls.ORDER.pop(0)
+        log_output('Selecting all items for job {}.'.format(selected_job))
+        items = cls.ITEMS[selected_job]
+        del cls.ITEMS[selected_job]
+        return selected_job, items
+
+    def process(self, item):
+        current_items = item['item_name'].split('\0')
+        job, items = self.handle_items(current_items, item.log_output)
+        item['item_job'] = job
+        item['item_name'] = '\0'.join(items)
+
+
+class GetJobConfig(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'GetJobConfig')
+
+    def process(self, item):
+        prop = 'job:{}:config'.format(item['item_job'])
+        config = requests.get(
+            'https://legacy-api.arpa.li/recursive/props',
+            params={
+                'prop': prop
+            }
+        ).json()[prop]
+        item['job_config'] = json.dumps(json.loads(config))
+        item.log_output('Got config for job {}.'.format(item['item_job']))
+
+
+class SetBadUrls(SimpleTask):
+    def __init__(self):
+        SimpleTask.__init__(self, 'SetBadUrls')
+
+    def process(self, item):
+        item['item_name_original'] = item['item_name']
+        items = item['item_name'].split('\0')
+        items_lower = [item['item_job']+':'+normalize_url(url) for url in json.loads(item['job_urls'])]
+        with open('%(item_dir)s/%(warc_file_base)s_bad-items.txt' % item, 'r') as f:
+            for url in {normalize_url(url) for url in f}:
+                index = items_lower.index(url)
+                items.pop(index)
+                items_lower.pop(index)
+        item['item_name'] = '\0'.join([s for s in items])
+
+
+def normalize_url(url, with_job=False):
+    if with_job:
+        job, url = url.split(':', 1)
+    url = url.split('#')[0]
+    while True:
+        temp = unquote(url).strip().lower()
+        if temp == url:
+            break
+        url = temp
+    if url.count('/') < 3:
+        url += '/'
+    url = url.split('/', 3)
+    if '@' in url[2]:
+        url[2] = url[2].split('@')[-1]
+    if ':' in url[2]:
+        url[2] = url[2].split(':')[0]
+    url = '/'.join(url)
+    if with_job:
+        url = job + ':' + url
+    return url
+
+
+class MaybeSendDoneToTracker(SendDoneToTracker):
+    def enqueue(self, item):
+        if len(item['item_name']) == 0:
+            return self.complete_item(item)
+        return super(MaybeSendDoneToTracker, self).enqueue(item)
+
+
+def get_hash(filename):
+    with open(filename, 'rb') as in_file:
+        return hashlib.sha1(in_file.read()).hexdigest()
+
+CWD = os.getcwd()
+PIPELINE_SHA1 = get_hash(os.path.join(CWD, 'pipeline.py'))
+LUA_SHA1 = get_hash(os.path.join(CWD, 'recursive.lua'))
+
+def stats_id_function(item):
+    d = {
+        'pipeline_hash': PIPELINE_SHA1,
+        'lua_hash': LUA_SHA1,
+        'python_version': sys.version,
+    }
+
+    return d
+
+
+class WgetArgs(object):
+    def realize(self, item):
+        wget_args = [
+            WGET_AT,
+            #'-U', USER_AGENT,
+            '-nv',
+            #'--no-cookies',
+            '--host-lookups', 'dns',
+            '--hosts-file', '/dev/null',
+            '--resolvconf-file', '/dev/null',
+            '--dns-servers', '9.9.9.10,149.112.112.10,2620:fe::10,2620:fe::fe:10',
+            '--reject-reserved-subnets',
+            #'--prefer-family', ('IPv4' if 'PREFER_IPV4' in os.environ else 'IPv6'),
+            '--content-on-error',
+            '--lua-script', 'recursive.lua',
+            '-o', ItemInterpolation('%(item_dir)s/wget.log'),
+            '--no-check-certificate',
+            '--output-document', ItemInterpolation('%(item_dir)s/wget.tmp'),
+            '--truncate-output',
+            '-e', 'robots=off',
+            '--recursive', '--level=inf',
+            '--no-parent',
+            '--page-requisites',
+            '--timeout', '30',
+            '--connect-timeout', '1',
+            '--tries', 'inf',
+            '--domains', 'numerabilis.u-paris.fr,biusante.parisdescartes.fr',
+            '--span-hosts',
+            '--waitretry', '30',
+            '--warc-file', ItemInterpolation('%(item_dir)s/%(warc_file_base)s'),
+            '--warc-header', 'operator: Archive Team',
+            '--warc-header', 'x-wget-at-project-version: ' + VERSION,
+            '--warc-header', 'x-wget-at-project-name: ' + TRACKER_ID,
+            '--warc-dedup-url-agnostic',
+            '--impersonate', 'firefox148-h1',
+            '--header', 'Accept-Encoding: identity'
+        ]
+
+        if '--concurrent' in sys.argv:
+            concurrency = int(sys.argv[sys.argv.index('--concurrent')+1])
+        else:
+            concurrency = os.getenv('CONCURRENT_ITEMS')
+            if concurrency is None:
+                concurrency = 2
+        item['concurrency'] = str(concurrency)
+
+        wget_args.extend(['--warc-header', 'job: '+item['item_job']])
+
+        item['job_urls'] = []
+
+        for item_name in item['item_name'].split('\0'):
+            wget_args.extend(['--warc-header', 'x-wget-at-project-item-name: '+item_name])
+            wget_args.append('item-name://'+item_name)
+            item_job, item_url = item_name.split(':', 1)
+            assert item['item_job'] == item_job
+            item['job_urls'].append(item_url)
+            wget_args.append(item_url)
+
+        item['job_urls'] = json.dumps(item['job_urls'])
+
+        item['item_name_newline'] = item['item_name'].replace('\0', '\n')
+
+        if 'bind_address' in globals():
+            wget_args.extend(['--bind-address', globals()['bind_address']])
+            print('')
+            print('*** Wget will bind address at {0} ***'.format(
+                globals()['bind_address']))
+            print('')
+
+        return realize(wget_args, item)
+
+###########################################################################
+# Initialize the project.
+#
+# This will be shown in the warrior management panel. The logo should not
+# be too big. The deadline is optional.
+project = Project(
+    title=TRACKER_ID,
+    project_html='''
+        <img class="project-logo" alt="Project logo" src="" height="50px" title="https://wiki.archiveteam.org/images/thumb/f/f3/Archive_team.png/235px-Archive_team.png"/>
+        <h2>Recursive Warrior Crawls <span class="links"><a href="https://tracker.archiveteam.org/recursive/">Leaderboard</a> &middot; <a href="https://wiki.archiveteam.org/index.php/Recursive_Warrior_Crawls">Wiki</a></span></h2>
+        <p>Running recursive Warrior crawls.</p>
+    '''
+)
+
+pipeline = Pipeline(
+    CheckIP(),
+    GetItemFromTracker('https://{}/{}/multi={}/'
+        .format(TRACKER_HOST, TRACKER_ID, MULTI_ITEM_SIZE),
+        downloader, VERSION),
+    SortAndSelectItems(),
+    GetJobConfig(),
+    PrepareDirectories(warc_prefix=TRACKER_ID),
+    WgetDownload(
+        WgetArgs(),
+        max_tries=1,
+        accept_on_exit_code=[0, 4, 8],
+        env={
+            'item_dir': ItemValue('item_dir'),
+            'item_names': ItemValue('item_name_newline'),
+            'warc_file_base': ItemValue('warc_file_base'),
+            'concurrency': ItemValue('concurrency'),
+            'item_job': ItemValue('item_job'),
+            'job_config': ItemValue('job_config'),
+            'job_urls': ItemValue('job_urls')
+        }
+    ),
+    SetBadUrls(),
+    PrepareStatsForTracker(
+        defaults={'downloader': downloader, 'version': VERSION},
+        file_groups={
+            'data': [
+                ItemInterpolation('%(item_dir)s/%(warc_file_base)s.warc.gz')
+            ]
+        },
+        id_function=stats_id_function,
+    ),
+    MoveFiles(),
+    LimitConcurrent(NumberConfigValue(min=1, max=20, default='20',
+        name='shared:rsync_threads', title='Rsync threads',
+        description='The maximum number of concurrent uploads.'),
+        UploadWithTracker(
+            'https://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+            downloader=downloader,
+            version=VERSION,
+            files=[
+                ItemInterpolation('%(data_dir)s/%(warc_file_base)s.warc.gz'),
+                ItemInterpolation('%(data_dir)s/%(warc_file_base)s_data.txt')
+            ],
+            rsync_target_source_path=ItemInterpolation('%(data_dir)s/'),
+            rsync_extra_args=[
+                '--recursive',
+                '--min-size', '1',
+                '--no-compress',
+                '--compress-level', '0'
+            ]
+        ),
+    ),
+    MaybeSendDoneToTracker(
+        tracker_url='https://%s/%s' % (TRACKER_HOST, TRACKER_ID),
+        stats=ItemValue('stats')
+    )
+)
---
+++ recursive.lua
@@ -0,0 +1,376 @@
+local urlparse = require("socket.url")
+local http = require("socket.http")
+local https = require("ssl.https")
+local cjson = require("cjson")
+
+local item_dir = os.getenv("item_dir")
+local warc_file_base = os.getenv("warc_file_base")
+local concurrency = tonumber(os.getenv("concurrency"))
+local job_urls = cjson.decode(os.getenv("job_urls"))
+local item_job = os.getenv("item_job")
+local job_config = cjson.decode(os.getenv("job_config"))
+local item_name = nil
+local item_url = nil
+
+local url_count = 0
+local tries = 0
+local downloaded = {}
+local addedtolist = {}
+local abortgrab = false
+local killgrab = false
+local logged_response = false
+
+local discovered = {
+  ["accepted"] = {},
+  ["not_accepted"] = {},
+  ["rejected"] = {}
+}
+local bad_items = {}
+
+local retry_url = false
+
+abort_item = function(item)
+  abortgrab = true
+  --killgrab = true
+  if not item then
+    item = item_name
+  end
+  if not bad_items[item] then
+    io.stdout:write("Aborting item " .. item .. ".\n")
+    io.stdout:flush()
+    bad_items[item] = true
+  end
+end
+
+kill_grab = function(item)
+  io.stdout:write("Aborting crawling.\n")
+  killgrab = true
+end
+
+read_file = function(file)
+  if file then
+    local f = assert(io.open(file))
+    local data = f:read("*all")
+    f:close()
+    return data
+  else
+    return ""
+  end
+end
+
+processed = function(url)
+  if downloaded[url] or addedtolist[url] then
+    return true
+  end
+  return false
+end
+
+discover_item = function(target, item)
+  item = item_job .. ":" .. item
+  if not discovered[target][item] then
+    for _, key in pairs({"not_accepted", "rejected"}) do
+      if target == "accepted"
+        and discovered[key][item] then
+        discovered[key][item] = nil
+      elseif target == key
+        and discovered["accepted"][item] then
+        return false
+      end
+    end
+print("discovered", target, item)
+    discovered[target][item] = true
+    return true
+  end
+  return false
+end
+
+report_bad_item = function(item)
+  if item ~= nil then
+    bad_items[item] = true
+  end
+end
+
+normalize_url = function(url)
+  local candidate_current = string.match(url, "^([^#]+)")
+  while true do
+    local temp = string.lower(urlparse.unescape(candidate_current))
+    if temp == candidate_current then
+      break
+    end
+    candidate_current = temp
+  end
+  if not string.match(candidate_current, "^https?://[^/]+/") then
+    candidate_current = candidate_current .. "/"
+  end
+  for _, pattern in pairs({
+    "^(https?://)[^/]*@([^/]+/.*)$",
+    "^(https?://[^/:]+):[^/]+(.*)$"
+  }) do
+    local a, b = string.match(candidate_current, pattern)
+    if a and b then
+      candidate_current = a .. b
+    end
+  end
+  return candidate_current
+end
+
+local urls = {}
+for _, url in pairs(job_urls) do
+  urls[normalize_url(url)] = true
+end
+
+set_item = function(url)
+  local candidate_current = normalize_url(url)
+  if candidate_current ~= item_url and urls[candidate_current] then
+    item_url = candidate_current
+    item_name = item_job .. ":" .. item_url
+  end
+end
+
+maybe_accept = function(candidate, accept)
+  for _, pattern in pairs(job_config["reject"] or {}) do
+    if string.match(candidate, pattern) then
+      discover_item("rejected", candidate)
+      return "rejected"
+    end
+  end
+  for _, pattern in pairs(job_config["~reject"] or {}) do
+    if not string.match(candidate, pattern) then
+      discover_item("rejected", candidate)
+      return "rejected"
+    end
+  end
+  if accept then
+    discover_item("accepted", candidate)
+    return "accepted"
+  end
+end
+
+discovery_check = function(url, parenturl)
+  for _, pattern in pairs(job_config["accept"] or {}) do
+    if string.match(url, pattern) then
+      if maybe_accept(url, true) then
+        return true
+      end
+    end
+  end
+  for _, pattern in pairs(job_config["~accept"] or {}) do
+    if not string.match(url, pattern) then
+      if maybe_accept(url, true) then
+        return true
+      end
+    end
+  end
+
+  if parenturl then
+    for parent_pattern, patterns in pairs(job_config["accept_with_parent"] or {}) do
+      if string.match(parenturl, parent_pattern) then
+        if type(patterns) == "string" then
+          patterns = {patterns}
+        end
+        for _, pattern in pairs(patterns) do
+          if string.match(url, pattern) then
+            if maybe_accept(url, true) then
+              return true
+            end
+          end
+        end
+      end
+    end
+  end
+
+  discover_item("not_accepted", url)
+
+  return false
+end
+
+wget.callbacks.download_child_p = function(urlpos, parent, depth, start_url_parsed, iri, verdict, reason)
+  local url = urlpos["url"]["url"]
+  local parenturl = parent and parent["url"] or nil
+
+  if job_config["accept_refresh"]
+    and urlpos["link_refresh_p"] ~= 0 then
+    maybe_accept(url, true)
+  end
+
+  if job_config["accept_inline"]
+    and urlpos["link_inline_p"] ~= 0 then
+    maybe_accept(url, true)
+  end
+
+  discovery_check(url, parenturl)
+
+  return false
+end
+
+bad_code = function(status_code)
+  return status_code ~= 200
+    and status_code ~= 301
+    and status_code ~= 302
+    and status_code ~= 303
+    and status_code ~= 307
+    and status_code ~= 308
+    and status_code ~= 404
+    and status_code ~= 410
+end
+
+wget.callbacks.write_to_warc = function(url, http_stat)
+  local status_code = http_stat["statcode"]
+  set_item(url["url"])
+  url_count = url_count + 1
+  io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+  io.stdout:flush()
+  logged_response = true
+  if not item_name then
+    error("No item name found.")
+  end
+  if bad_code(http_stat["statcode"]) then
+    print("Not writing to WARC.")
+    return false
+  elseif http_stat["statcode"] ~= 200 then
+    return true
+  end
+  if abortgrab then
+    print("Not writing to WARC.")
+    return false
+  end
+  retry_url = false
+  tries = 0
+  return true
+end
+
+wget.callbacks.httploop_result = function(url, err, http_stat)
+  local status_code = http_stat["statcode"]
+
+  if not logged_response then
+    url_count = url_count + 1
+    io.stdout:write(url_count .. "=" .. status_code .. " " .. url["url"] .. " \n")
+    io.stdout:flush()
+  end
+  logged_response = false
+
+  if killgrab then
+    return wget.actions.ABORT
+  end
+
+  set_item(url["url"])
+  if not item_name then
+    error("No item name found.")
+  end
+
+  if abortgrab then
+    abort_item()
+    return wget.actions.EXIT
+  end
+
+  if status_code >= 300 and status_code <= 399 then
+    local newloc = nil
+    if http_stat["newloc"] then
+      newloc = urlparse.absolute(url["url"], http_stat["newloc"])
+    end
+    if newloc and (
+      status_code == 301
+      or status_code == 303
+      or status_code == 308
+    ) then
+      discovery_check(newloc, url["url"])
+      return wget.actions.EXIT
+    end
+  end
+
+  if downloaded[url["url"]] then
+    report_bad_item(item_name)
+    return wget.actions.EXIT
+  end
+
+  if bad_code(status_code) then
+    io.stdout:write("Server returned " .. http_stat.statcode .. " (" .. err .. ").\n")
+    io.stdout:flush()
+    report_bad_item(item_name)
+    return wget.actions.EXIT
+  end
+
+  if status_code >= 200 and status_code <= 399 then
+    downloaded[url["url"]] = true
+  end
+
+  local sleep_time = 0
+
+  if sleep_time > 0.001 then
+    os.execute("sleep " .. sleep_time)
+  end
+
+  tries = 0
+
+  return wget.actions.NOTHING
+end
+
+wget.callbacks.finish = function(start_time, end_time, wall_time, numurls, total_downloaded_bytes, total_download_time)
+  local function submit_backfeed(items, key)
+    local tries = 0
+    local maxtries = 5
+    while tries < maxtries do
+      if killgrab then
+        return false
+      end
+      local body, code, headers, status = http.request(
+        "https://legacy-api.arpa.li/backfeed/legacy/" .. key,
+        items .. "\0"
+      )
+      if code == 200 and body ~= nil and cjson.decode(body)["status_code"] == 200 then
+        io.stdout:write(string.match(body, "^(.-)%s*$") .. "\n")
+        io.stdout:flush()
+        return nil
+      end
+      io.stdout:write("Failed to submit discovered URLs." .. tostring(code) .. tostring(body) .. "\n")
+      io.stdout:flush()
+      os.execute("sleep " .. math.floor(math.pow(2, tries)))
+      tries = tries + 1
+    end
+    kill_grab()
+    error()
+  end
+
+  local file = io.open(item_dir .. "/" .. warc_file_base .. "_bad-items.txt", "w")
+  for url, _ in pairs(bad_items) do
+    file:write(url .. "\n")
+  end
+  file:close()
+  local targets = {}
+  for name, key in pairs(job_config["keys"]) do
+    targets["recursive-" .. item_job .. "-" .. name .. "-" .. key .. "?shard=job:" .. item_job .. ":" .. name] = discovered[name]
+  end
+  for key, data in pairs(targets) do
+    print("queuing for", string.match(key, "^([^%?]+)%-"))
+    local items = nil
+    local count = 0
+    for item, _ in pairs(data) do
+      print("found item", item)
+      if items == nil then
+        items = item
+      else
+        items = items .. "\0" .. item
+      end
+      count = count + 1
+      if count == 1000 then
+        submit_backfeed(items, key)
+        items = nil
+        count = 0
+      end
+    end
+    if items ~= nil then
+      submit_backfeed(items, key)
+    end
+  end
+end
+
+wget.callbacks.before_exit = function(exit_status, exit_status_string)
+  if killgrab then
+    return wget.exits.IO_FAIL
+  end
+  if abortgrab then
+    abort_item()
+  end
+  return exit_status
+end
+
